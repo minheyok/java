75f7724f9bcfc6cf3ebbfddeb04c95db106c483d
wkdalsgur5556 <wkdalsgur5556@gmnail.com>
2025-01-03 15:42:27 +0900

    ThisJava

---
+++ ThisJava/src/ch01/sec10/Hello.java
@@ -0,0 +1,19 @@
+package ch01.sec10;
+
+  //교재 p30 실습
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+    System.out.println("Hello, Java");
+    
+    
+    
+    
+    
+    
+	}
+
+}
