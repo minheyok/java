c9d26bee6f433e6c60be486cadd177f8c8e67fca
wkdalsgur5556 <wkdalsgur5556@gmnail.com>
2025-01-06 11:50:09 +0900

    add ch04

--- ch04/src/sub2/MethodTest.java
+++ ch04/src/sub2/MethodTest.java
@@ -17,15 +17,34 @@ public class MethodTest {
 		System.out.println("y1 :" +y1);
 		System.out.println("y2 :" +y2);
 		System.out.println("y3 :" +y3);
-	}
+		
+		// 지역변수와 메서드
+		int t1 =sum(1, 10);
+		int t2 =sum(1, 100);
+		
+		System.out.println("t1 :" + t1);
+		System.out.println("t2 :" + t2);
+		
+	}   //main 메서드 종료
 	
 	// 메서드 정의
-	public static int fx(int x) {  // main 메서드에서 호출해야하기 때문에 main과 같이 static를 키워드를 맞춰줌
+	// main 메서드에서 호출해야하기 때문에 main과 같이 static를 키워드를 맞춰줌
+	
+	public static int fx(int x) { //접근권한, 반환타입, 메서드명(매게변수)
 		
 		int y= 2*x+3;
 		
 		return y;
-		
 	}
-
+		//메서드 정의
+		public static int sum(int start, int end) {
+			
+			int tota1 =0; //지역변수 : 메서드내에 선언한 변수,메서드가 종료되면 변수 해제
+			
+			for(int k=start ; k<=end ; k++) {
+				tota1 += k;
+			}
+			
+			return tota1;	
+	}
 }
---
+++ ch04/src/sub2/MethodTypeTesy.java
@@ -0,0 +1,64 @@
+package sub2;
+/*
+ * 날짜 : 2025/01/06
+ * 이름 : 장민혁
+ * 내용 : Java 메서드 타입 실습하기
+ */
+public class MethodTypeTesy { 
+	
+	public static void main(String[] args) {
+	
+		// 메서드 호출
+	double y1 = type1(1.2); // 인자값 : 메서드를 홏풀할때 전달하는 값
+	double y2 = type1(3.4);
+	
+	System.out.println("y1 :" +y1);
+	System.out.println("y2 :" +y2);
+	
+	type2(true);  //반환값이 없기 때문에 메서드 옆에 대입연산(=) 없음
+	type2(false);
+
+	boolean result =type3();
+	System.out.println("result :" +result);
+	
+		
+	}
+	
+	// 타입1 :매게변수 O 반환값 O
+	public static double type1(double r) {
+	
+	double y = (r *r) + 3.14;
+	  return y;
+	}
+	// 타입2 :매게변수 O 반환값 X
+	public static void type2(boolean status) {
+		if(status) {
+			System.out.println("참 입니다.");
+		}else {
+			System.out.println("거짓 입니다.");
+		}
+		
+	}
+	// 타입3 :매게변수 X 반환값 O
+	public static boolean type3() {
+		int num1 =1;
+		int num2 =2;
+		
+		if(num1 > num2) {
+		}else {
+				return false;
+			
+		}
+		
+	}
+	
+		
+	// 타입4 :매게변수 X 반환값 X
+	public static void type4() {
+		
+		double result = type1(2.3);
+		System.out.println("반지름 2.3인 ");
+		
+		
+	}
+}
