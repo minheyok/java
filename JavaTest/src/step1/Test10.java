18e387a62a594dfe4c61a9f1359c2421daf299cc
wkdalsgur5556 <wkdalsgur5556@gmnail.com>
2025-01-06 14:01:07 +0900

    JavaTest

--- JavaTest/src/step1/Test10.java
+++ JavaTest/src/step1/Test10.java
@@ -12,7 +12,17 @@ public class Test10 {
 		int a =sc.nextInt();
 		int b =sc.nextInt();
 		
-		int c = 
+		int c1 = (b%10)*a;
+		int c2=(b/10)%10*a;
+		int c3 = (b/100)*a;
+		int sum = a*b;
+		
+		System.out.println(c1);
+		System.out.println(c2);		
+		System.out.println(c3);
+		System.out.println(sum);
+		
+		
 	}
 
 }
--- JavaTest/src/step1/Test12.java
+++ JavaTest/src/step1/Test12.java
@@ -5,6 +5,9 @@ public class Test12 {
 	public static void main(String[] args) {
 		
 		
+		System.out.print("\\    /\\,)  ( '),(  /  )\\(__)|");
+		
+		
 		
 	}
 
