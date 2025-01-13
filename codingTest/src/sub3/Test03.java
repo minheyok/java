002869fd67c1ce0ff289f025077caf3b20a24148
wkdalsgur5556 <wkdalsgur5556@gmnail.com>
2025-01-13 17:50:42 +0900

    add codintTest

---
+++ codingTest/src/sub3/Test03.java
@@ -0,0 +1,20 @@
+package sub3;
+
+import java.util.Scanner;
+
+public class Test03 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int sum=0;
+		
+		for(int i=1; i<=n ; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+	}
+
+}
---
+++ codingTest/src/sub3/Test04.java
@@ -0,0 +1,15 @@
+package sub3;
+
+import java.util.Scanner;
+
+public class Test04 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+	}
+
+}
